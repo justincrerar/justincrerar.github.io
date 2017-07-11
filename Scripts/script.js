6ab9100855e485dcfb4e2a54e2f2ee0d604ef6a1
justincrerar <justin_crerar@my.uri.edu>
2017-07-11 10:47:53 -0400

    Getting it started

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $(".flip:first").next(".panel").slideDown("slow");
+    $(".flip").click(function () {
+        $(".panel").slideUp('slow');
+        if ($(this).next(".panel").is(':visible')) {
+            $(this).next(".panel").slideUp("slow");
+        }
+        else {
+            $(this).next(".panel").slideDown("slow");
+        }
+    });
+});
