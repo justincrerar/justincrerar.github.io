2667f0eca999403db384016360dfaa8763b73657
IGotAGuy <gibsonsg032@gmail.com>
2017-08-10 00:51:49 -0400

    Fixed Javascripting. Collapsing all for now

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,39 +1,16 @@
-﻿$(document).ready(function () {
-    $(".flip:first").next(".panel").slideDown("slow");
-    $(".flip").click(openflip($(this)));
-    var skillNav = document.getElementById("skillLink");
-    skillNav.addEventListener("click", tellMe, false);
-    /*skillNav.click(function () {
-        console.log("Running open skills");
-        openFlip(document.getElementById("skillPanel"));
-    });*/
-    var storyNav = document.getElementById("storyLink");
-    storyNav.click(function () {
-        console.log("Running open story");
-        openFlip(document.getElementById("storyPanel"));
-    });
-    /*<li class="navbar-brand" id="skillLink">Skills</li>
-        <li class="navbar-brand" id="storyLink">My Story</li>
-        <li class="navbar-brand" id="expLink">Experience</li>
-        <li class="navbar-brand" id="contactLink">Links and Contact</li>*/
-    function openSkills() {
-        console.log("Running open skills");
-        openFlip(document.getElementById("skillPanel"));
-    };
-
-    function tellMe() {
-        alert("Hey");
-    }
-
-    function openFlip(flipObject) {
-        //Slide up all panels
-        $(".panel").slideUp('slow');
-
-        if (flipObject.next(".panel").is(':visible')) {
-            flipObject.next(".panel").slideUp("slow");
-        }
-        else {
-            flipObject.next(".panel").slideDown("slow");
-        }
-    }
+﻿/*
+*When updating the javascript, don't forget to change the v value to refresh
+*/
+$(document).ready(function () {
+    $(".panel").slideDown('slow');
+    //$(".flip:first").next(".panel").slideDown("slow");
+    //$(".flip").click(function () {
+    //    $(".panel").slideUp('slow');
+    //    if ($(this).next(".panel").is(':visible')) {
+    //        $(this).next(".panel").slideUp("slow");
+    //    }
+    //    else {
+    //        $(this).next(".panel").slideDown("slow");
+    //    }
+    //});
 });
