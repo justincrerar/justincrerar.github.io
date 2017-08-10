5a81602a636b5880d526c9cfc02ea2458a18de61
IGotAGuy <gibsonsg032@gmail.com>
2017-08-10 12:57:57 -0400

    Added scripting and html for bootstrap navbar

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,7 @@
 *When updating the javascript, don't forget to change the v value to refresh
 */
 $(document).ready(function () {
+    //$("#headshotDiv").fadeIn('slow');
     $(".panel").slideDown('slow');
     //$(".flip:first").next(".panel").slideDown("slow");
     //$(".flip").click(function () {
@@ -13,4 +14,11 @@ $(document).ready(function () {
     //        $(this).next(".panel").slideDown("slow");
     //    }
     //});
+    $('ul.nav').find('a').click(function () {
+        var $href = $(this).attr('href');
+        var $anchor = $('#' + $href).offset();
+        $('body').animate({ scrollTop: $anchor.top - 10 }, "slow");
+        return false;
+    });
 });
+
